b813bf5c1c1e96c2037ac60acd07cf903daa6f42
patrickpowns <patrick.melanstone@gmail.com>
2012-09-05 13:40:21 -0400

    first commit

---
+++ encryption1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+
+    char key[7], text[61];
+    ifstream keyFile;
+    keyFile.open ("key.txt");
+    if (keyFile.is_open())
+        cout << "key file has been opened\n";
+
+    keyFile >> key;
+
+
+
+    return 0;
+}
