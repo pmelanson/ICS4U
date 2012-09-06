b6a2143bc0964d09bf123788c75dda627c75472c
patrickpowns <patrick.melanstone@gmail.com>
2012-09-06 15:00:11 -0400

    more work on encryption

--- unit 1/encryption1.cpp
+++ unit 1/encryption1.cpp
@@ -1,18 +1,43 @@
 #include <iostream>
 #include <fstream>
+#include <ctype.h>
 using namespace std;
 
 int main() {
 
-    char key[7], text[61];
-    ifstream keyFile;
-    keyFile.open ("key.txt");
-    if (keyFile.is_open())
-        cout << "key file has been opened\n";
+    int i = 0, n = 0;
+    char cipher[7], plaintext[61], ciphertext[61];
+    ifstream inFile;
 
-    keyFile >> key;
+    inFile.open ("cipher.txt");
+    if (inFile.is_open())
+        cout << "cipher file has been opened\n";
+
+    inFile.getline (cipher, 7);
+    cout << cipher << endl;
+    inFile.close();
+
+    inFile.open ("message.txt");
+    if (inFile.is_open())
+        cout << "message file has been opened\n";
+
+    inFile.getline (plaintext, 61);
+    cout << plaintext << endl;
+    inFile.close();
+/*
+
+8======================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================D
+
+*/
 
 
+    //converts message to ciphertext, ignoring non-alpha characters
+    for (i = 0; i < strlen(plaintext); i++)
+        if (isalpha (plaintext[i]))
+//            ciphertext[n] = plaintext[i] + (cipher[n++ % strlen(cipher)] - 'A');  //converts from plaintext to ciphertext
+            ciphertext[i] = plaintext[i];  //converts from plaintext to ciphertext
+
+    cout << ciphertext;
 
     return 0;
 }
