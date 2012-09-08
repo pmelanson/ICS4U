91f0005681182fd2c1e1904718c1cf960db6804f
Patrick Melanson <patrickpowns@gmail.com>
2012-09-08 08:53:10 -0400

    poop

--- unit 1/encryption1.cpp
+++ unit 1/encryption1.cpp
@@ -5,39 +5,39 @@ using namespace std;
 
 int main() {
 
-    int i = 0, n = 0;
-    char cipher[7], plaintext[61], ciphertext[61];
-    ifstream inFile;
+	int i = 0, n = 0;
+	char cipher[7] = "", plaintext[61] = "", ciphertext[61] = "";
+	ifstream inFile;
 
-    inFile.open ("cipher.txt");
-    if (inFile.is_open())
-        cout << "cipher file has been opened\n";
+	inFile.open ("cipher.txt");
+	if (inFile.is_open())
+		cout << "cipher file has been opened\n";
 
-    inFile.getline (cipher, 7);
-    cout << cipher << endl;
-    inFile.close();
+	inFile.getline (cipher, 7);
+	cout << cipher << endl;
+	inFile.close(), inFile.clear();
 
-    inFile.open ("message.txt");
-    if (inFile.is_open())
-        cout << "message file has been opened\n";
+	inFile.open ("message.txt");
+	if (inFile.is_open())
+		cout << "message file has been opened\n";
 
-    inFile.getline (plaintext, 61);
-    cout << plaintext << endl;
-    inFile.close();
-/*
+	inFile.getline (plaintext, 61);
+	cout << plaintext << endl;
+	inFile.close(), inFile.clear();
 
-8======================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================D
+	/*
 
-*/
+	8======================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================================D
 
+	*/
 
-    //converts message to ciphertext, ignoring non-alpha characters
-    for (i = 0; i < strlen(plaintext); i++)
-        if (isalpha (plaintext[i]))
-//            ciphertext[n] = plaintext[i] + (cipher[n++ % strlen(cipher)] - 'A');  //converts from plaintext to ciphertext
-            ciphertext[i] = plaintext[i];  //converts from plaintext to ciphertext
+	//converts message to ciphertext, ignoring non-alpha characters
+	for (i = 0; i < strlen(plaintext); i++)
+		if (isalpha(plaintext[i]) )
+			ciphertext[n] = plaintext[i] + (cipher[n++ % strlen(cipher)] - 'A');  //converts from plaintext to ciphertext (ciphertext character = plaintext character + apropriate cipher character, alpha value. increment n.
+//            ciphertext[i] = plaintext[i];  //converts from plaintext to ciphertext
 
-    cout << ciphertext;
+	cout << "Encrypted message: " << ciphertext << endl;
 
-    return 0;
+	return 0;
 }
