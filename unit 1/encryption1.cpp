055b3f59332170b43d6e9d623df3ccf987622c56
Patrick Melanson <patrickpowns@gmail.com>
2012-09-10 18:47:38 -0400

    FINALLY FRIGGEN DONE THAT WAS UNREASONABLY LONG

--- unit 1/encryption1.cpp
+++ unit 1/encryption1.cpp
@@ -1,50 +1,34 @@
 //Program by Patrick Melanson, ICS4U
-
 #include <iostream>
 #include <fstream>
 #include <ctype.h>
 using namespace std;
-
 int main() {
 
     //initialization
     int i = 0, n = 0;
     char cipher[7], plaintext[61], ciphertext[61];
+    char encrypted;
     ifstream inFile;
-
-    //open file
     inFile.open ("message.txt");
-    if (inFile.is_open())
-        cout << "file has been opened\n";
 
-    //read cipher
-    inFile.getline (cipher, 7);
+    inFile.getline (cipher, 7);	//read cipher
     cout << cipher << endl;
-
-    //read plaintext
-    inFile.getline (plaintext, 61);
+    inFile.getline (plaintext, 61);	//read plaintext
     cout << plaintext << endl;
 
-    //cleanup
-    inFile.close(), inFile.clear();
+    do	//encrypts message using cipher
+		if (isupper (plaintext[i])) {	//checks if the char is an uppercase letter
+			encrypted = plaintext[i] + cipher[n % strlen(cipher)] - 'A';  //encrypt
+			while (encrypted > 'Z')	encrypted -= 26;	//deals with out-of-ranges
+		ciphertext[n++] = encrypted;	//assigns encrypted char to string
+	}
+	while (plaintext[i++]);
+	ciphertext[n] = '\0';	//finishes up with string
 
-    //close file
-    inFile.close(), inFile.clear();
+	cout << "\n\nCiphertext: " << ciphertext << endl;	//displays encrypted message
 
-    //converts message to ciphertext, ignoring non-alpha characters
-    while (plaintext[i]){
-		if (isupper (plaintext[i])){
-    		ciphertext[n] = (plaintext[i] + cipher[n % strlen(cipher)] - 'A') % 'Z';
-    		cout << char ((plaintext[i] + cipher[n % strlen(cipher)] - 'A') % 'Z');
-		    n++;
-		}
-		i++;
-    }
-
-    ciphertext[n] = '\0';
-
-    cout << "\n\nCiphertext: " << ciphertext << endl;
-
+	inFile.close(), inFile.clear();	//finishes up with file
     system("pause");
     return 0;
 }
