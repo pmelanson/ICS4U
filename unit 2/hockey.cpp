573eb36009e1412e12bb16cf93e1121586908d55
patrickpowns <patrickpowns@gmail.com>
2012-09-12 14:58:50 -0400

    started hockey assignment

---
+++ unit 2/hockey.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main() {
+
+	parse();
+	average();
+	sort();
+
+
+
+
+	system("pause");
+	return 0;
+}
