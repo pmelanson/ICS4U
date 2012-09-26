5ede54c1ef1dc798e3ac105723ffa802b11e89b5
patrickpowns <patrickpowns@gmail.com>
2012-09-26 15:02:30 -0400

    I like this sorting

--- unit 2/hockey.cpp
+++ unit 2/hockey.cpp
@@ -48,16 +48,17 @@ struct player_t {
 
 	player_t (string _name, unsigned short _height, unsigned short _weight, unsigned short _day, unsigned short _month, unsigned _year, unsigned short _age) :	//constructor
 		name (_name), height (_height), weight (_weight), birthday (_day, _month, _year), age (_age) {
-/*		cout << name;
-		if (name.length() > 15)
-			cout << "\t" << height;
-		else
-			cout << "\t\t" << height;
-		cout << "\t\t" << weight;
-		cout << "\t\t", birthday.printBirthday();
-		cout << "\t" << age;
-		cout << endl;
-*/
+		///uncomment this if you want players to print their data when constructed///
+		/*		cout << name;
+				if (name.length() > 15)
+					cout << "\t" << height;
+				else
+					cout << "\t\t" << height;
+				cout << "\t\t" << weight;
+				cout << "\t\t", birthday.printBirthday();
+				cout << "\t" << age;
+				cout << endl;
+		*/
 	}
 
 
@@ -89,9 +90,9 @@ bool parseLine (istream &stream) {
 		string readHeight;
 		if (getline (line, readHeight, ',')) {	//reads height until the next comma (in ft'in" format)
 			istringstream heightStream (readHeight);
-			heightStream.ignore (1, '"');
+			heightStream.ignore (2, '"');
 			heightStream >> feet;
-			heightStream.ignore (1, '\'');
+			heightStream.ignore (2, '\'');
 			heightStream >> inches;
 		} else
 			return false;
@@ -146,14 +147,16 @@ void average (vector <player_t*> dataset, float &height, float &weight, float &a
 
 bool getTeam (string teamName) {
 
-	ifstream file (string(teamName+".txt").c_str());	//adds .txt to teamName
+	ifstream file (string(teamName+".txt").c_str());	//adds .txt to teamName, opens the team's datafile
 
 	while (parseLine (file));	//reads all players
 	float height=0, weight=0, age=0;
-	average (roster, height, weight, age);
-	roster.clear();	//cleans up for the next time getTeam will be called
 
-	league.push_back (new team_t(teamName, height, weight, age));	//creates a new team
+	average (roster, height, weight, age);
+	league.push_back (new team_t(teamName, height, weight, age));	//creates a new team, with the calculated data
+
+	roster.clear();	//cleans up for the next time getTeam will be called
+	file.close(), file.clear();	//closes file
 
 	return true;
 }
@@ -163,20 +166,12 @@ bool setup() {
 	chdir("AllTeams");
 	system("dir > Zdirectory.txt /b");
 
-//	cout << "    Player Name\t\t" << "Height (in.)\t" << "Weight (lbs.)\t" << "Birthdate\t" << "Age\n\n";
-
-//	cout << "Enter team name (should also be file name): ";
-//	cin >> teamName;
-//	fileName = teamName + ".txt";
-//	ifstream file (fileName.c_str());
-//	if (file.good())
-//		cout << "opened " << fileName << endl;
+//	cout << "    Player Name\t\t" << "Height (in.)\t" << "Weight (lbs.)\t" << "Birthdate\t" << "Age\n\n";	//uncomment this if you want to display player stats as they are read in
 
 	cout << "\n\n\nTeam Name\t" << "Height (in.)\t" << "Weight (lbs.)\t" << "Age\n\n";
 
-	string teamName ("");
-
 	ifstream directory ("Zdirectory.txt");
+	string teamName ("");
 
 	do {
 		getline (directory, teamName, '.');	//read a teamname in from .directory.txt, continuing until the first '.' is reached, at which point...
@@ -185,19 +180,96 @@ bool setup() {
 	} while (getTeam (teamName));	//gets team stats from file
 
 	directory.close();
-
 	system("del Zdirectory.txt");
 
 	return true;
 }
 
+void sortHeight(vector <team_t*> &dataset) {
+
+	vector <team_t*>::iterator it, selected, compare = league.begin();	//declares iterators
+
+/* 	for (it = league.begin(); it < --league.end(); ++it) {
+ *
+ * 		for (compare = ++league.begin(); compare < league.end(); ++compare)	//goes through the entire vector
+ * 			if ((*compare)->height < (*selected)->height)	//finds the smallest height
+ * 				selected = compare;	//if a smaller height than the one already selected is found, select the smaller height
+ *
+ * 		iter_swap((*it), (*selected));
+ * 	}
+ */
+
+}
+
+void sortWeight(vector <team_t*> &dataset) {
+
+	vector <team_t*>::iterator it, selected, compare = league.begin();	//declares iterators
+
+//	for (it = league.begin(); it < --league.end(); ++it) {
+//
+//		for (compare = ++league.begin(); compare < league.end(); ++compare)	//goes through the entire vector
+//			if ((*compare)->weight < (*selected)->weight)	//finds the smallest weight
+//				selected = compare;	//if a smaller weight than the one already selected is found, select the smaller weight
+//
+//		iter_swap((*it), (*selected));
+//	}
+
+}
+
+void sortAge(vector <team_t*> &dataset) {
+
+	vector <team_t*>::iterator it, selected, compare = league.begin();	//declares iterators
+
+/* 	for (it = league.begin(); it < --league.end(); ++it) {
+ *
+ * 		for (compare = ++league.begin(); compare < league.end(); ++compare)	//goes through the entire vector
+ * 			if ((*compare)->age < (*selected)->age)	//finds the smallest age
+ * 				selected = compare;	//if a smaller age than the one already selected is found, select the smaller age
+ *
+ * 		iter_swap((*it), (*selected));
+ * 	}
+ */
+
+}
+
+bool sort(vector <team_t*> dataset) {
+
+	cout << "\n\nWhat do you want to do now?\n";
+	cout << "[1] Sort by height\n";
+	cout << "[2] Sort by weight\n";
+	cout << "[3] Sort by age\n";
+	cout << "> ";
+
+	unsigned short choice;
+	cin >> choice;
+
+	switch (choice) {
+	case 1:
+		sortHeight(dataset);
+		break;
+
+	case 2:
+		sortWeight(dataset);
+		break;
+
+	case 3:
+		sortAge(dataset);
+		break;
+
+	default:
+		cout << "Invalid input";
+		return false;
+	}
+}
 
 int main() {
 
 	if (setup()) {
 
-//		sort();
-
+		if (sort(league))	//runs sort
 		return 0;
-	} else return 1;
+		else
+		return 1;	//if sorting fails
+
+	} else return 2;
 }
