2ee517ef81c6f5ae7385a091f969d562aea96c74
patrickpowns <patrickpowns@gmail.com>
2012-10-24 13:47:08 -0400

    poop

--- unit 5/MELANSON_Patrick_stackalc/include/stack.cpp
+++ unit 5/MELANSON_Patrick_stackalc/include/stack.cpp
@@ -1,9 +1,16 @@
 #include "stack.h"
 
-stack_t::push(float x) {
+void stack_t::push(float x) {
 	if (top != 19) data[++top] = x;
 }
 
-stack_t::pop() {
+float stack_t::pop() {
 	return (top != -1) ? data[top--] : 0;
+}
+
+float stack_t::peek() {
+	return (top != -1) ? data[top] : 0;
+}
+
+stack_t::stack_t() : top (-1) {
 }
--- unit 5/MELANSON_Patrick_stackalc/stackalc.cpp
+++ unit 5/MELANSON_Patrick_stackalc/stackalc.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <sstream>
+#include "include\stack.h"
 using namespace std;
 
 
